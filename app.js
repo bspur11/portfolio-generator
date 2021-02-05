c0fc2e5e74557fb01a7dea37cea201131b9b3c77
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-02-05 09:15:30 -0500

    adding html functionality git status

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template.js');
 const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 
 const promptUser = () => {
   return inquirer.prompt([{
@@ -132,13 +133,20 @@ Add a New Project
 
 };
 
+
+
+
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      //   console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
-
-
 
 
 
